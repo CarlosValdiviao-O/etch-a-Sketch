039a9ce6825595f1fc2f95e3eb17ba56ca5b6092
CarlosValdiviao-O <carlosvaldivia390@gmail.com>
2022-03-25 16:55:46 -0400

    Pick color, Opacity and Rainbow buttons added

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const DEFAULT_COLOR = 'black';
-const DEFAULT_MODE = 'color';
+const DEFAULT_COLOR = 'rgb(0, 0, 0)';
 const DEFAULT_SIZE = 10;
 
 let num = DEFAULT_SIZE;
 let color = DEFAULT_COLOR;
-let sizeChangeAux = 10;
-let rainbowMode = true;
+const container = document.getElementsByClassName('container');
+
+let colorPicked = document.querySelector('#colorPicker');
+colorPicked.addEventListener('input', () => color = colorPicked.value);
+let colorButton = document.querySelector('#color');
+colorButton.addEventListener('click', () => rainbowMode = false);
+
+let opacityMode = false;
+let opacity = document.querySelector('#opacity');
+opacity.addEventListener('click', () => opacityMode ? opacityMode = false: opacityMode = true);
+
+let rainbowMode = false;
+let rainbowButton = document.querySelector('#rainbow');
+rainbowButton.addEventListener('click', () => (rainbowMode) ? rainbowMode = false: rainbowMode = true );
 
 let slider = document.querySelector("#slider");
+slider.addEventListener('input', updateSizeBox);
+
 let sliderBox = document.querySelector("#sliderBox");
 sliderBox.addEventListener('mouseup',updateSize);
-slider.addEventListener('input', updateSizeBox);
+
 let size = document.querySelector('#sizeBox');
-const container = document.getElementsByClassName('container');
+let sizeChangeAux = 10;
+
 let toggle = document.querySelector('#toggleGrid');
 toggle.addEventListener('click', toggleGrid);
 let toggleAux = false;
+
 createDivs (num);
 
 function createDivs (num) {
@@ -34,10 +49,6 @@ function createDivs (num) {
             column[i].appendChild(block);
         }
     }
-}
-
-function stopDrag(e){
-    e.type='mouseover';
 }
 
 function updateSizeBox() {
@@ -60,18 +71,50 @@ function toggleGrid (){
 }
 
 function changeColor(e){
-    if (e.type === 'mouseover' && !mouseDown) return ;
-    if (rainbowMode) {
-        let red = Math.floor(Math.random() * 255);
-        let green = Math.floor(Math.random() * 255);
-        let blue = Math.floor(Math.random() * 255);
-        e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    if (e.type === 'mouseover' && !clicked) return ;
+    
+    if (opacityMode) { 
+        if (e.target.style.backgroundColor.substr(0,4) == 'rgba') e.target.style.backgroundColor = increaseOpacity(e.target.style.backgroundColor);
+        else e.target.style.backgroundColor = toRGBA(e.target.style.backgroundColor);  
+    
+        if (rainbowMode && !e.target.style.backgroundColor) {
+        e.target.style.backgroundColor = toRainbow();
+        }
+    
+        else if (!e.target.style.backgroundColor){
+            e.target.style.backgroundColor = color;
+        }
     }
+
     else{
-        e.target.style.backgroundColor = color;
+        if (rainbowMode) {
+            e.target.style.backgroundColor = toRainbow();
+        }
+        else if (!e.target.style.backgroundColor){
+            e.target.style.backgroundColor = color;
+        }
     }
 }
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+function toRainbow(){
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+//hasta aqui funciona
+function toRGBA (str) {
+    let newStr = `rgba(` + str.substring(4, (str.length-1)) + `, 0.1)`;
+    return newStr;
+}
+
+function increaseOpacity (str) {
+    let newOpacity = str.substring(str.length-4, str.length-1);
+    newOpacity = (+newOpacity*10 + 1)/10;
+    let newStr = str.substring(0, str.length-4) + newOpacity + `)`;
+    return newStr;
+}
+
+let clicked = false;
+document.body.onmousedown = () => (clicked = true);
+document.body.onmouseup = () => (clicked = false);
